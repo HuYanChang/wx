64b3ff5baa3ddc423dda1741eebb2cb888cfdcdd
hyc <976262957@qq.com>
2018-09-15 18:22:19 +0800

    test

--- app/helper/function.php
+++ app/helper/function.php
@@ -18,3 +18,76 @@ if(!function_exists('arrayToStr')){
         return $queryStr;
     }
 }
+
+if(!function_exists('httpGet')){
+    function httpGet($url, $dataType){
+        $oCurl = curl_init();
+        if(stripos($url, "https://")!==FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+        }
+        if($dataType) {
+            $header[] = 'Content-Type: application/x-www-form-urlencoded';
+            curl_setopt($oCurl,CURLOPT_HTTPHEADER,$header);
+        } else {
+            $header[] = 'Content-Type:application/json;charset=utf-8';
+            curl_setopt($oCurl,CURLOPT_HTTPHEADER,$header);
+        }
+
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if(intval($aStatus["http_code"])==200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+    }
+}
+
+if(!function_exists('httpPost')){
+    function httpPost($url, $param, $data_typ=false, $post_file=false){
+        $oCurl = curl_init();
+        if(stripos($url,"https://")!==FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+        }
+        if (is_string($param) || $post_file) {
+            $strPOST = $param;
+        } else {
+            $aPOST = array();
+            foreach($param as $key=>$val){
+                $aPOST[] = $key."=".urlencode($val);
+            }
+            $strPOST =  join("&", $aPOST);
+        }
+
+        if($data_typ == 2) {
+            $header[] = 'Content-Type: application/x-www-form-urlencoded';
+            curl_setopt($oCurl,CURLOPT_HTTPHEADER,$header);
+        } else {
+            if($data_typ) {
+                $header[] = 'Content-Type:application/json;charset=utf-8';
+                curl_setopt($oCurl,CURLOPT_HTTPHEADER,$header);
+            }
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($oCurl, CURLOPT_POST,true);
+        curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+        if($post_file) curl_setopt($oCurl, CURLOPT_NOBODY, true);
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+
+        if(intval($aStatus["http_code"])==200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+    }
+}

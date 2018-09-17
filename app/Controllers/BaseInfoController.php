1b46e9c62c76ff086c251721c9f8feb7d68a7d86
hyc <976262957@qq.com>
2018-09-17 14:00:33 +0800

    bug

--- app/Controllers/BaseInfoController.php
+++ app/Controllers/BaseInfoController.php
@@ -9,7 +9,7 @@ namespace app\Controllers;
 use wxphp\base\Controller;
 use lib\FormatDataTrait;
 
-class baseInfoController extends Controller{
+class BaseInfoController extends Controller{
     use FormatDataTrait;
     const BASEURL = 'https://api.weixin.qq.com';
     const APPID = 'wxe35be9a4ca8325f9';
@@ -21,7 +21,7 @@ class baseInfoController extends Controller{
     public function getCode2session()
     {
         $code = $_POST('code');
-        if(empty($code)) (new FormatData())->responseDataFormat(40035);
+        if(empty($code)) return $this->responseDataFormat(40035);
         $getOpenIdUrl = getBaseInfo::BASEURL.'/sns/jscode2session?';
         $data = array(
             'appid' => getBaseInfo::APPID,
@@ -32,7 +32,15 @@ class baseInfoController extends Controller{
         $getStr = arrayToStr($data);
         $getUrl = $getOpenIdUrl.$getStr;
         $code2Session = httpGet($getUrl);
-        return $this->responseDataFormat(200);
+        if($code2Session['errcode'] !==0){
+            $errcode = $code2Session['errcode'];
+            $data = [];
+            var_dump($code2Session);
+        }else{
+            $errcode = 200;
+            $data = $code2Session;
+        }
+        return $this->responseDataFormat($errcode, $data);
         // TODO: Implement getSessionKey() method.
     }
 }

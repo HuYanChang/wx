5c4123e9de91b42449ed322255f47464b784574d
hyc <976262957@qq.com>
2018-10-15 14:32:23 +0800

    bug

--- app/Models/BookModel.php
+++ app/Models/BookModel.php
@@ -20,7 +20,7 @@ class BookModel extends Model{
         if(empty($checkUserInfo)) return 50001;
         //要查询的字段FROM_UNIXTIME(create_time, '%Y-%m-%d %H:%i') as create_time
 
-        $col = ['total_money', 'purchase_price', 'goods_count', 'nick_name', 'create_time'];
+        $col = ['order_id', 'total_money', 'purchase_price', 'goods_count', 'nick_name', 'create_time'];
         parent::where('wx_book', array('user_id = '.$checkUserInfo['u_id']), [], $col);
         $bookList = parent::fetch();
         if(!empty($bookList)) $data = $bookList;

d62d5a6bf99e5626292500a6d00f2623d68c530d
hyc <976262957@qq.com>
2018-10-19 14:59:04 +0800

    coupon

--- app/Controllers/CouponController.php
+++ app/Controllers/CouponController.php
@@ -20,12 +20,20 @@ class CouponController extends Controller{
         $this->couponModel = new CouponModel();
 
     }
+
+    /**
+     * @author hyc
+     * @FunDEsc:获取优惠券列表
+     */
     public function couponList(){
         $unionId = isset($_GET['union_id'])?$_GET['union_id']: '';
         if(empty($unionId)) $this->responseDataFormat(10003);
+        $isUse = isset($_GET['is_use'])? $_GET['is_use']: 0;
         $page = isset($_GET['page'])? $_GET['page']: 1;
         $limit = isset($_GET['limit'])? $_GET['limit']: 10;
         $offset = ($page - 1)*$limit;
-        $errcode = $this->couponModel->couponList($unionId, $offset, $limit, self::$data);
+        $errcode = $this->couponModel->couponList($unionId, $offset, $limit, $isUse, self::$data);
+        if($errcode !== 200) $this->responseDataFormat($errcode);
+        $this->responseDataFormat($errcode, self::$data);
     }
 }
--- app/Models/CouponModel.php
+++ app/Models/CouponModel.php
@@ -14,13 +14,33 @@ class CouponModel extends Model{
     protected $table = 'wx_coupon';
     protected $limit = 10;
     protected $offset = 0;
+    const ERRCODE = 200;
 
-    public function couponList($unionId, $offset, $limit, &$data){
+    /**
+     * @author hyc
+     * @FunDesc:获取优惠券列表
+     * @param $unionId
+     * @param $offset
+     * @param $limit
+     * @param $isUse
+     * @param $data
+     * @return int
+     */
+    public function couponList($unionId, $offset, $limit, $isUse, &$data){
         $this->limit = $limit;
         $this->offset = $offset;
         $baseInfoModel = new BaseInfoModel();
         $checkUser = $baseInfoModel->checkUserExist($unionId);
         if(empty($checkUser)) return 50001;
-
+        $col = ['coupon_name', 'coupon_expire', 'coupon_type', 'coupon_rule'];
+        if($isUse){
+            $where = array('user_id = '.$checkUser['user_id'], 'and ', 'status = 2');
+        }else{
+            $where = array('user_id = '.$checkUser['user_id']);
+        }
+        parent::where($this->table, $where,[], $col);
+        $couponList = parent::querySql('', 1);
+        if(!empty($couponList)) $data = $couponList;
+        return self::ERRCODE;
     }
 }
